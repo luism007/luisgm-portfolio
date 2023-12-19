ec54a4074b3d998909a53e60026fc104be476cf7
luism007 <luisgm.w001@gmail.com>
2023-12-19 11:38:45 -0800

    Fixed weird scroll-to-bottom bug. Also fixed a minor scale issue with Enter button

--- components/styled-components/parallax-wrapper/ParallaxWrapper.tsx
+++ components/styled-components/parallax-wrapper/ParallaxWrapper.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './ParallaxWrapper.scss';
 
 export const ParallaxWrapper = ({children}) => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    })
     return (
         <div className = "parallax-wrapper">
             {children}
