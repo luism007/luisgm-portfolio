e0bdeacee27e693343ad3887aa1d59ed30a474ef
luism007 <luisgm.w001@gmail.com>
2023-12-20 14:47:38 -0800

    Added fixes for scrolling issues in Safari browser.

--- sections/start/Start.tsx
+++ sections/start/Start.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import './Start.scss';
 import LuisHeadshot from '../../assets/luis-headshot.svg';
 import LuisTitle from '../../assets/luisgm-title.svg';
+// import LuisLogo from '../../assets/luisgm-title.png';
 import { useScrollTo } from "../../hooks/useScrollTo";
 import { JumpSectionButton } from "../../components/styled-components/jump-to-section-button/JumpSectionButton";
 import DownArrowLight from '../../assets/down-arrow-light.svg';
@@ -12,6 +13,7 @@ export const Start = () => {
         <div className="section-container start" id = "start">
                 <img src={LuisHeadshot} className="parallax-background" />
                 <img src={LuisTitle} className="parallax-foreground" />
+                {/* <img src={LuisLogo} className="parallax-foreground" /> */}
                 <JumpSectionButton callback={scrollTo} title="Enter" icon= {DownArrowLight} theme= {ETheme.LIGHT}></JumpSectionButton>
         </div>
     )
