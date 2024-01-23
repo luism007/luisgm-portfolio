9ab2f332b05fcb9f9faf8d171edfc1754cd93fc5
luism007 <luisgm.w001@gmail.com>
2024-01-23 14:04:11 -0800

    Adding JumpToSectionButton component to Experience section for jumping to Projects section

--- components/styled-components/jump-to-section-button/JumpSectionButton.tsx
+++ components/styled-components/jump-to-section-button/JumpSectionButton.tsx
@@ -3,7 +3,7 @@ import './JumpSectionButton.scss';
 import { ETheme } from "../../../constants/enums/ETheme";
 export const JumpSectionButton: React.FC<{callback: () => void, title: string, icon: string, theme: ETheme }> = (props) => {
     return(
-        <div onClick={props.callback} className="jump-to-section-button-container">
+        <div onClick={props.callback} className={`jump-to-section-button-container ${props.theme}`}>
             <h3 className= {props.theme}> {props.title} </h3>
         </div>
     )
